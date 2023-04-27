64dfc7491fb99e784988866a5b396aef02a48f25
Benjamin Bryant <bryantbc98@gmail.com>
2023-04-27 13:00:16 -0400

    added a bunch of images and styling to flesh out about page and fix some issues with index.html

--- assets/script.js
+++ assets/script.js
@@ -1,43 +1,149 @@
 const repairModels = $(".repair-models");
-const modelModal = $("#repair-modal");
+const repairModal = $("#repair-modal");
 const tigImg = $("#tig-img");
 const urImg = $("#ur-img");
 const classProgress = $(".progress");
+const panelNav = $(".panel");
 
-repairModels.on("click", function (event) {
+repairModels.on("click", ".is-clickable", function (event) {
   event.preventDefault();
 
-  generateModelModal(event.target.dataset.company);
+  generaterepairModal(event.target.dataset.company);
+  $("html").attr("style", "overflow:hidden;");
 });
 
-$("#exit").on("click", function (event) {
+$(".exit").on("click", function (event) {
   event.preventDefault();
 
   let currentModal = $(".modal");
   currentModal.removeClass("is-active");
+  $("html").removeAttr("style");
+  $("a").remove(".temp-block");
 });
 
-const generateModelModal = (company) => {
+$("#model-exit").on("click", function (event) {
+  event.preventDefault();
+
+  $("#model-modal").removeClass("is-active");
+});
+
+// Generates modal information based on selected button
+const generaterepairModal = (company) => {
   let companyName = "";
+  let modelList = [];
   switch (company) {
     case "hp":
       companyName = "Hewlett-Packard";
+      modelList = hpModels;
       break;
     case "dell":
       companyName = "Dell";
+      modelList = dellModels;
       break;
     case "lenovo":
       companyName = "Lenovo";
+      modelList = lenovoModels;
       break;
     case "acer":
       companyName = "Acer";
+      modelList = acerModels;
   }
 
-  modelModal.find(".panel-heading").text(`${companyName} Models`);
+  repairModal.find(".panel-heading").text(`${companyName} Models`);
+  generatePanelBlocks(modelList, companyName);
 
-  modelModal.addClass("is-active");
+  repairModal.addClass("is-active");
 };
 
+// Model Lists
+const hpModels = [
+  "Chromebook 11 G6 EE",
+  "Chromebook 11A G6 EE",
+  "Chromebook 11 G7 EE",
+  "Chromebook 11 G8 EE",
+  "Chromebook 11A G8 EE",
+  "ProBook 640 G1",
+  "ProBook 645 G1",
+  "ProBook 650 G1",
+  "ProBook 650 G2",
+  "ProBook 655",
+];
+
+const dellModels = [
+  "Chromebook 3100",
+  "Chromebook 3110",
+  "Chromebook 3110 2-in-1",
+  "Latitude 5430 Chromebook",
+  "Latitude 5430 2-in-1 Chromebook",
+  "Latitude 3300",
+  "Latitude 3330",
+  "Latitude 3390 2-in-1",
+  "Latitude 3400",
+  "Latitude 3550",
+];
+
+const lenovoModels = [
+  "100e Chromebook 2nd Gen",
+  "300e 1st Gen",
+  "300e 2nd Gen",
+  "E40-80",
+  "ThinkPad 11e G1",
+  "ThinkPad 11e G2",
+  "ThinkPad 11e G3",
+  "ThinkPad 11e G4",
+  "ThinkPad 11e G5",
+  "ThinkPad T-590",
+];
+
+const acerModels = [
+  "Travelmate B113-E",
+  "Travelmate B113-M",
+  "TravelMate B115-MP",
+  "TravelMate B3 B311",
+  "TravelMate Spin B118-G2-RN",
+  "Aspire 3 A315",
+  "Aspire E3-111",
+  "Aspire One 11 AO1-132",
+  "Aspire V5 131",
+  "R3-131T",
+];
+
+function Device(model, company, image, specs) {
+  this.model = model;
+  this.company = company;
+  this.image = image;
+  this.specs = specs;
+}
+
+const generatePanelBlocks = (models, company) => {
+  models.forEach((model) => {
+    const modelObjects = [];
+    let newDevice = new Device(
+      model,
+      company,
+      `./images/${company}/img/${model}.png`,
+      `./images/${company}/specs/${model}.png`
+    );
+
+    let newBlock = `<a class="panel-block temp-block" data-img="${newDevice.image}" data-specs="${newDevice.specs}">
+          <span class="panel-icon">
+            <i class="fas fa-laptop"></i>
+          </span>${model}
+        </a>`;
+    panelNav.append(newBlock);
+
+    modelObjects.push(newDevice);
+  });
+};
+
+$(".panel").on("click", ".temp-block", function (event) {
+  let modelModal = $("#model-modal");
+  $(".model-img").attr("src", event.target.dataset.img);
+  $(".model-specs").attr("src", event.target.dataset.specs);
+  modelModal.addClass("is-active");
+});
+
+// Opens new window with link to image website
 tigImg.on("click", function () {
   window.open("https://www.tig.com/");
 });
@@ -46,6 +152,7 @@ urImg.on("click", function () {
   window.open("https://bootcamps.richmond.edu/coding/");
 });
 
+// Class Progress Bar updates as we get nearer to graduation date
 const startDate = new Date("February 13, 2023");
 const graduationDate = new Date("Aug 7, 2023");
 const today = new Date();
