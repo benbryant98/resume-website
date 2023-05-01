96c163b640300d1209e82d9b4c7b1316f055a17c
Benjamin Bryant <bryantbc98@gmail.com>
2023-04-30 22:47:30 -0400

    added functionality to carousel and tweaked some other js changed bootstrap css pathing

--- (public)/assets/js/bootstrapScript.js
+++ (public)/assets/js/bootstrapScript.js
@@ -23,3 +23,52 @@ $("#accordionExperience").on("click", ".accordion-button", (e) => {
     $(e.target).removeClass("collapsed");
   }
 });
+
+let classStart = new Date("02/03/2023");
+let classEnd = new Date("08/07/2023");
+let today = new Date();
+let classLength = classEnd - classStart;
+
+let progress = ((today - classStart) / classLength) * 100;
+$(".progress-bar").attr("style", `width: ${progress}%`);
+
+let count = 0;
+let picArray = $(".carousel-inner").children($(".carousel-item"));
+let picIndicator = $('.carousel-indicators').children();
+
+$(".carousel-control-prev-icon").on("click", (e) => {
+  e.preventDefault();
+  let picArray = $(".carousel-inner").children($(".carousel-item"));
+
+  $(picArray[count]).removeClass("active");
+  $(picIndicator[count])
+
+  if (count > 0) {
+    count--;
+  } else {
+    count = picArray.length - 1;
+  }
+
+  $(picArray[count]).addClass("active");
+});
+
+$(".carousel-control-next-icon").on("click", (e) => {
+  e.preventDefault();
+  
+  $(picArray[count]).removeClass("active");
+  if (count < picArray.length - 1) {
+    count++;
+  } else {
+    count = 0;
+  }
+  
+  $(picArray[count]).addClass("active");
+});
+
+$('.carousel-indicators').on('click', 'button', (e) => {
+  picIndicator.removeClass('active');
+  picArray.removeClass('active');
+  $(e.target).addClass('active');
+  $(picArray[e.target.dataset.bsSlideTo]).addClass('active');
+  count = e.target.dataset.bsSlideTo;
+})
